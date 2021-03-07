47478f30493fbbc61825de83b73947aa7328a122
asvarnon <austinvarnon@gmail.com>
2021-03-06 23:18:32 -0600

    adding note

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,11 +3,15 @@ boardRow1 = [' ', ' ', ' ']
 boardRow2 = [' ', ' ', ' ']
 boardRow3 = [' ', ' ', ' ']
 
+
+#Makes Gameboard
 def gameBoard(row1, row2, row3):
     print(row1)
     print(row2)
     print(row3)
 
+
+#Gets position
 def getPosition():
     userRowInput = int(input("Choose a row: "))
     userIndexInput = int(input("Choose an index position: "))
@@ -16,3 +20,6 @@ def getPosition():
 userRowInput, userIndexInput = getPosition()
 print(userRowInput)
 print(userIndexInput)
+
+
+

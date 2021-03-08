70c872bde8d1620e3f064f883a3fdf4445f6be02
asvarnon <austinvarnon@gmail.com>
2021-03-08 13:50:12 -0600

    annotated and cleaned validations

--- TicTacToe.py
+++ TicTacToe.py
@@ -11,22 +11,49 @@ def gameBoard(row1, row2, row3):
     print(row3)
 
 
-#Gets position
+#Gets positions
+
 def getRow():
-    userRowInput = input("Choose a row (1-3): ")
-    while userRowInput.isdigit() == False:
+    #initial
+    userRowInput = ""
+    acceptableValues = range(1,4)
+    withinRange = False
+
+    #Two Conditons to check, isDigit & within Range
+    while userRowInput.isdigit() == False or withinRange == False:
         userRowInput = input("Choose a row (1-3): ")
+        #Digit Check
         if not userRowInput.isdigit():
             print("Sorry, that is not a digit.")
+        #Range Check
+        if userRowInput.isdigit():
+            if int(userRowInput) in acceptableValues:
+                withinRange = True
+            else:
+                print("Not in Acceptable Range")
+                pass
     return int(userRowInput)
         
 
 def getIndex():
-    userIndexInput = input("Choose an index position (1-3): ")
-    while userIndexInput.isdigit() == False:
+    #initial
+    userIndexInput = ""
+    acceptableValues = range(1,4)
+    withinRange = False
+
+    #Two Conditons to check, isDigit & within Range
+    while userIndexInput.isdigit() == False or withinRange == False:
         userIndexInput = input("Choose a row (1-3): ")
+        #Digit Check
         if not userIndexInput.isdigit():
             print("Sorry, that is not a digit.")
+        #Range Check
+        if userIndexInput.isdigit():
+            if int(userIndexInput) in acceptableValues:
+                withinRange = True
+            else:
+                print("Not in Acceptable Range")
+                pass
     return int(userIndexInput)
 
 userRowInput = getRow()

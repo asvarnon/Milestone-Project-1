3f778f0e80a743c4fe0367eada66e258ad179a69
asvarnon <austinvarnon@gmail.com>
2021-03-25 11:44:15 -0500

    figured out better way to assign sides using tuples

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,4 @@
-
+from IPython.display import clear_output
 
 gameList = ['1','2','3','4','5','6','7','8','9']
 
@@ -18,18 +18,23 @@ def playerSides():
             return player1, player2
     
 # print(playerSides())
-player1 = playerSides()[0]
-player2 = playerSides()[1]
+# player1 = playerSides()[0]
+# player2 = playerSides()[1]
 
 # print(player1)
 # print(player2)
 
 
 def displayGame(gameList):
-    print("Here is the current List:")
-    print(gameList)
+    clear_output()
+    print("Here is the current board:")
+    print(gameList[6]+' | '+gameList[7]+' | '+gameList[8])
+    print('----------')
+    print(gameList[3]+' | '+gameList[4]+' | '+gameList[5])
+    print('----------')
+    print(gameList[0]+' | '+gameList[1]+' | '+gameList[2])
+    
 
-def playGame():
-    return null
+displayGame(gameList)
 
 
